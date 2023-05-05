6bdd6462780d6d6e6c6fa6e6d985b4421a23da9d
mentamarindos <mentamarino@gmail.com>
2023-05-05 03:27:56 -0600

    create company model and migration

---
+++ database/migrations/2023_05_05_092056_create_companies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id(); // ID de la compañía
+            $table->string('name'); // Nombre de la compañía
+            $table->string('email'); // Correo electrónico de la compañía
+            $table->timestamps(); // Fecha de creación y actualización
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+    
+};
